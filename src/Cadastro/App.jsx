744df3ff5d1a636ee4f08cf721ae4ab54028d0b4
Guilherme França <guilhermefran8@gmail.com>
2023-05-02 17:33:12 -0300

    Criado o componente FormCadastro.jsx

--- src/Cadastro/App.jsx
+++ src/Cadastro/App.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import React, { useRef } from 'react';
 import NavBar from '../Apresentacao/components/NavBar'
 import Footer from '../Apresentacao/components/Footer'
+import FormCadastro from './components/FormCadastro';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <h1>TELA DE CADASTRO</h1>
+      <FormCadastro />
       <Footer />
     </div>
   )
